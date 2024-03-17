f5e3ed91c8bece9308fb8312edcb21ffbde262ab
JordyV23 <jordyvargas0123@gmail.com>
2024-03-16 19:06:05 -0600

    Creacion de rutas
    - Estudio del ruteo en angular
    - Creacion de rutas padre y rutas hijas
    - Creacion de componentes
    - Creacion de paginas

---
+++ 03-country-app/src/app/shared/pages/aboutPage/aboutPage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-about-page',
+  templateUrl: './aboutPage.component.html',
+  styleUrls: []
+})
+export class AboutPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ 03-country-app/src/app/shared/pages/contactPage/contactPage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-contact-page',
+  templateUrl: './contactPage.component.html',
+  styleUrls: []
+})
+export class ContactPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
