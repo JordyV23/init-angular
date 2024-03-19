59fa7f5117ffe0da685fc99f075b11eec6494e4d
JordyV23 <jordyvargas0123@gmail.com>
2024-03-19 05:18:16 -0600

    Rutas y Componentes
     - Creacion de rutas hijas
    - creacion de componente para busqueda

---
+++ 03-country-app/src/app/shared/components/searchBox/searchBox.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+})
+export class SearchBoxComponent {
+  @ViewChild('txtInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  @Input()
+  public placeholder: string = '';
+
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
+}
