15c960fe4d09e8e1b81b69661a0e512e3cde45fd
JordyV23 <jordyvargas0123@gmail.com>
2024-07-31 07:27:21 -0600

    Se estudia el uso de pipes de fecha mas avanzados y configuracion de idiomas

--- 04-pipes-app/src/app/app.module.ts
+++ 04-pipes-app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -7,6 +7,16 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
 
+// Configuracion del Locale
+import localeEsHN from "@angular/common/locales/es-HN";
+import localeDeDU from "@angular/common/locales/de";
+import localeFrCa from "@angular/common/locales/fr-CA";
+import { registerLocaleData } from "@angular/common";
+
+// El uso de un local, afecta el formato de los pipes
+registerLocaleData(localeEsHN)
+registerLocaleData(localeDeDU)
+registerLocaleData(localeFrCa)
 
 @NgModule({
   declarations: [
@@ -18,7 +28,9 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-HN' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
