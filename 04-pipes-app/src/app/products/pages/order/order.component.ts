2aca66524a1f695af5e64fdca745750950f17ae2
JordyV23 <jordyvargas0123@gmail.com>
2024-11-10 11:33:49 -0600

    Recambio
     - Se da pausa al proyecto 04
    - Se da inicio al proyecto 09 por temas de exploracion de tecnologia

---
+++ 04-pipes-app/src/app/products/pages/order/order.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
+
+
+@Component({
+  selector: 'products-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent {
+
+  public isUpperCase:boolean = false;
+
+  public heroes: Hero[] = [
+    {name:'Superman', canFly:true, color: Color.blue},
+    {name:'Batman', canFly:false, color: Color.black},
+    {name:'Greenlatern', canFly:true, color: Color.green},
+    {name:'Daredevil', canFly:false, color: Color.red},
+  ]
+
+  toggleUpperCase():void {
+    this.isUpperCase = !this.isUpperCase
+  }
+
+}
