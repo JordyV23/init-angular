cb79f765842acbdae529bbf76d3f9afe0ae51428
JordyV23 <jordyvargas0123@gmail.com>
2024-12-22 17:54:02 -0600

    Pipe personalizado

--- 04-pipes-app/src/app/products/pages/order/order.component.ts
+++ 04-pipes-app/src/app/products/pages/order/order.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Color, Hero } from '../../interfaces/hero.interface';
 
-
 @Component({
   selector: 'products-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+  styleUrls: ['./order.component.css'],
 })
 export class OrderComponent {
-
-  public isUpperCase:boolean = false;
+  public isUpperCase: boolean = false;
+  public orderBy: keyof Hero | null = 'canFly';
 
   public heroes: Hero[] = [
-    {name:'Superman', canFly:true, color: Color.blue},
-    {name:'Batman', canFly:false, color: Color.black},
-    {name:'Greenlatern', canFly:true, color: Color.green},
-    {name:'Daredevil', canFly:false, color: Color.red},
-  ]
+    { name: 'Superman', canFly: true, color: Color.blue },
+    { name: 'Batman', canFly: false, color: Color.black },
+    { name: 'Greenlatern', canFly: true, color: Color.green },
+    { name: 'Daredevil', canFly: false, color: Color.red },
+  ];
 
-  toggleUpperCase():void {
-    this.isUpperCase = !this.isUpperCase
+  toggleUpperCase(): void {
+    this.isUpperCase = !this.isUpperCase;
   }
 
+  changeOrder(value: keyof Hero) {
+    this.orderBy = value;
+  }
 }
--- src/environments/environments.ts
+++
@@ -1,4 +0,0 @@
-
-export const environment = {
- mapbox_key: "pk.eyJ1Ijoiam9yZHl2MjMiLCJhIjoiY20zYzNsejBnMDlweTJpcG5wN3RvMGJ3byJ9.sKVS6U0NJT1ig0YJ5Odm_Q"
-};
