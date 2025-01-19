0c88182ae08c3afa9f15251ce9d8ea030f94ff52
JordyV23 <jordyvargas0123@gmail.com>
2025-01-18 20:40:24 -0600

    Avance en renderizado de heroes
     - Se agregan imagenes para rendereizar en cada heroe
    - Se agregan 2 funciones al servicio de heroes
    - Se crea la barra de busqueda
    - Se cambia la version de json-server

--- 05-heroes-app/src/app/heroes/services/heroes.service.ts
+++ 05-heroes-app/src/app/heroes/services/heroes.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
 @Injectable({ providedIn: 'root' })
 export class HeroesService {
-
   private baseUrl: string = environments.baseUrl;
 
   constructor(private http: HttpClient) {}
 
-  getHerores():Observable<Hero[]>{
-    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  getHerores(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
+  getHeroById(id: String): Observable<Hero | undefined> {
+    return this.http
+      .get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(catchError((error) => of(undefined)));
+  }
 
+  getSugestions(searchTerm: string): Observable<Hero[]> {
+    return this.http.get<Hero[]>(
+      `${this.baseUrl}/heroes?q=${searchTerm}&_limit=6`
+    );
+  }
 }
