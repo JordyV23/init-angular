5acb938b097be2696a97190db632d757f931ee22
JordyV23 <jordyvargas0123@gmail.com>
2025-02-01 22:31:44 -0600

    backend update

--- 05-heroes-app/src/app/heroes/services/heroes.service.ts
+++ 05-heroes-app/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -23,6 +23,22 @@ export class HeroesService {
   getSugestions(searchTerm: string): Observable<Hero[]> {
     return this.http.get<Hero[]>(
       `${this.baseUrl}/heroes?q=${searchTerm}&_limit=6`
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('Hero Id is required');
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHeroById(id: String): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError((err) => of(false)),
+      map((resp) => true)
     );
   }
 }
