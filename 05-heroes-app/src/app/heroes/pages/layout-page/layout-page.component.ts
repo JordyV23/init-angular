ed9862b5deb9f3bdcda6fb2ae2cd490e8c1c963b
JordyV23 <jordyvargas0123@gmail.com>
2025-02-24 21:53:44 -0600

    Avance de servicios

--- 05-heroes-app/src/app/heroes/pages/layout-page/layout-page.component.ts
+++ 05-heroes-app/src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class LayoutPageComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
   public sideBarItems = [
-    {label: 'Listado', icon:'label', url:'./list'},
-    {label: 'Añadido', icon:'add', url:'./new-hero'},
-    {label: 'Buscar', icon:'search', url:'./search'},
-  ]
+    { label: 'Listado', icon: 'label', url: './list' },
+    { label: 'Añadido', icon: 'add', url: './new-hero' },
+    { label: 'Buscar', icon: 'search', url: './search' },
+  ];
 
+  onLogout() {
+    this.authService.logout;
+    this.router.navigate(['/auth/login'])
+  }
+
+  get user(): User | undefined {
+    return this.authService.currentUser;
+  }
 }
